6a8f9c02bb87ea2edc8a8af473cf7d16d21e197f
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 00:48:47 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,26 @@
 # Write your code here.
+
+def dictionary(word)
+  substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+  "hello" becomes 'hi'
+  "to, two, too" become '2'
+  "for, four" become '4'
+  'be' becomes 'b'
+  'you' becomes 'u'
+  "at" becomes "@"
+  "and" becomes "&"
+# Method for finding all of the long dictionary keys in tweets and replacing those found words with their coresponding dictionary values
+def word_substituter(tweet)
+  
+end
