be4970b9d90672a5dc599946f9d77524bc73afae
Trevor Jameson <tjameson15@gmail.com>
2018-07-30 01:36:46 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -51,7 +51,7 @@ end
 def shortened_tweet_truncator(tweet)
   twit = selective_tweet_shortener(tweet)
   if twit.size > 140
-    puts twit.slice!(0..136)
+    twit.slice!(0..136)
     twit << "..."
   end
   twit
